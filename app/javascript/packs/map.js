65683c9f56f2a0314d90806cac1245c880329056
Alexandre Gros <xelaflash@gmail.com>
2018-03-30 12:29:37 +0200

    add address and googler map

---
+++ app/javascript/packs/map.js
@@ -0,0 +1,10 @@
+import GMaps from 'gmaps/gmaps.js';
+
+function initMap() {
+        // Create a map object and specify the DOM element for display.
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 52.5206, lng: 13.4098},
+          scrollwheel: false,
+          zoom: 8
+        });
+}
