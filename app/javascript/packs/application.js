a5ee6f0d35f3222cdf8b2a81f34b99519a402a68
Xelaflash <xelaflash@gmail.com>
2018-09-11 15:11:04 -0400

    review styling + add tab active class

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,11 @@
 import "bootstrap";
+
+const current = document.location.pathname.split('/')[2];
+const tabs = document.querySelectorAll('.tab');
+
+  Array.from(tabs).forEach(function(el) {
+   const truncatedHref = el.href.split('/')[4];
+   if (truncatedHref === current) {
+     el.classList.add('active');
+   }
+  });
