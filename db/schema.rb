009e3646f7acca6db78efefdba4b74b7f9bd1aaf
Xelaflash <xelaflash@gmail.com>
2019-08-21 09:01:26 -0400

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180304114832) do
+ActiveRecord::Schema.define(version: 2018_03_04_114832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
